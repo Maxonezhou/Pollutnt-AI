65a1005a83a9d15f5cd8379b3d88d2429f073ec3
PatrickPLi <lipatrick.1023@gmail.com>
2020-03-07 19:59:41 -0500

    Can't predict future vals but can comp validation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from solace import *
 import time
 import datetime as dt
 import threading
+import csv
 
 def parse_data(str):
     split_str = str.split(',')
@@ -26,8 +27,29 @@ print("Connected to Firebase")
 db = firebase.database()
 data_count = 0
 while True:
-    if data_count >= 10:
-        train_and_predict("CO2", False)
+    if data_count >= 2:
+        all_data = db.child("Data").get()
+        co2 = []
+        tvoc = []
+        pressure = []
+        temperature = []
+        humidity = []
+        for child in all_data.each():
+            # print(child.val().get("CO2"))
+            co2.append(child.val().get("CO2"))
+            tvoc.append(child.val().get("TVOC"))
+            pressure.append(child.val().get("Pressure"))
+            temperature.append(child.val().get("Temperature"))
+            humidity.append(child.val().get("Humidity"))
+        with open('dataset.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Time","CO2","TVOC","Pressure","Temperature","Humidity"])
+            for i in range(len(co2)):
+                writer.writerow([i,co2[i],tvoc[i],pressure[i],temperature[i],humidity[i]])
+            
+        predictions = train_and_predict("CO2", False, len(co2))
+        print(predictions)
+        data_count = 0
     while client.message_received != True:
         time.sleep(0.01)
     # print(client.message_contents)
